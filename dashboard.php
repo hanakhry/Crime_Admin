9fc112ee4ed65a5630ddc0dae0d5a9591c8eccbc
Hana Khoury <hanna.khoury@microfocus.com>
2021-09-13 13:41:45 +0300

    Fix

--- dashboard.php
+++ dashboard.php
@@ -102,7 +102,7 @@
                         <div class="row">
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Physical violence: 230</div>
+                                    <div class="card-body">Physical violence: 1230</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
                                         <a class="small text-white stretched-link" href="#">View Details</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
