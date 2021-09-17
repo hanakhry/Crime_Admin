4bc4f500ce3e3f8fca701212e6c6b0222468d8e5
Hana Khoury <hanna.kh89@gmail.com>
2021-09-17 20:55:00 +0300

    Version 4.2 fix

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function validate(){
 var username = document.getElementById("inputEmail").value;
 var password = document.getElementById("inputPassword").value;
 if ( username == "admin" && password == "admin"){
-alert ("Login successfully");
+
 window.location = "dashboard.php"; 
 return false;
 }
